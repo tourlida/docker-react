7951a037aedb0a6d0b4f88dd21ead439577e2928
Vagia Tourlida <vagia.tourlida@powerfactors.com>
2023-06-20 10:27:30 +0300

    initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
